49a3d27b8dd208a67212fe199899cfdb2aa2a517
jetaehyun <jetaehyun0@gmail.com>
2021-01-23 19:58:38 -0500

    state flags get updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,58 +1,54 @@
-states = {
-	  'Alabama': 'AL',
-	  'Alaska': 'AK',
-	  'Arizona': 'AZ',
-	  'Arkansas': 'AR',
-	  'California': 'CA',
-	  'Colorado': 'CO',
-	  'Connecticut': 'CT',
-	  'Delaware': 'DE',
-	  'Florida': 'FL',
-	  'Georgia': 'GA',
-	  'Hawaii': 'HI',
-	  'Idaho': 'ID',
-	  'Illinois': 'IL',
-	  'Indiana': 'IN',
-	  'Iowa': 'IA',
-	  'Kansas': 'KS',
-	  'Kentucky': 'KY',
-	  'Louisiana': 'LA',
-	  'Maine': 'ME',
-	  'Marshall Islands': 'MH',
-	  'Maryland': 'MD',
-	  'Massachusetts': 'MA',
-	  'Michigan': 'MI',
-	  'Minnesota': 'MN',
-	  'Mississippi': 'MS',
-	  'Missouri': 'MO',
-	  'Montana': 'MT',
-	  'Nebraska': 'NE',
-	  'Nevada': 'NV',
-	  'New Hampshire': 'NH',
-	  'New Jersey': 'NJ',
-	  'New Mexico': 'NM',
-	  'New York': 'NY',
-	  'North Carolina': 'NC',
-	  'North Dakota': 'ND',
-	  'Ohio': 'OH',
-	  'Oklahoma': 'OK',
-	  'Oregon': 'OR',
-	  'Palau': 'PW',
-	  'Pennsylvania': 'PA',
-	  'Puerto Rico': 'PR',
-	  'Rhode Island': 'RI',
-	  'South Carolina': 'SC',
-	  'South Dakota': 'SD',
-	  'Tennessee': 'TN',
-	  'Texas': 'TX',
-	  'Utah': 'UT',
-	  'Vermont': 'VT',
-	  'Virgin Islands': 'VI',
-	  'Virginia': 'VA',
-	  'Washington': 'WA',
-	  'West Virginia': 'WV',
-	  'Wisconsin': 'WI',
-	  'Wyoming': 'WY'
+var states = {
+	  'alabama': 'AL',
+	  'alaska': 'AK',
+	  'arizona': 'AZ',
+	  'arkansas': 'AR',
+	  'california': 'CA',
+	  'colorado': 'CO',
+	  'connecticut': 'CT',
+	  'delaware': 'DE',
+	  'florida': 'FL',
+	  'georgia': 'GA',
+	  'hawaii': 'HI',
+	  'idaho': 'ID',
+	  'illinois': 'IL',
+	  'indiana': 'IN',
+	  'iowa': 'IA',
+	  'kansas': 'KS',
+	  'kentucky': 'KY',
+	  'louisiana': 'LA',
+	  'maine': 'ME',
+	  'maryland': 'MD',
+	  'massachusetts': 'MA',
+	  'michigan': 'MI',
+	  'minnesota': 'MN',
+	  'mississippi': 'MS',
+	  'missouri': 'MO',
+	  'montana': 'MT',
+	  'nebraska': 'NE',
+	  'nevada': 'NV',
+	  'new hampshire': 'NH',
+	  'new jersey': 'NJ',
+	  'new mexico': 'NM',
+	  'new york': 'NY',
+	  'north carolina': 'NC',
+	  'north dakota': 'ND',
+	  'ohio': 'OH',
+	  'oklahoma': 'OK',
+	  'oregon': 'OR',
+	  'pennsylvania': 'PA',
+	  'rhode island': 'RI',
+	  'south carolina': 'SC',
+	  'south dakota': 'SD',
+	  'tennessee': 'TN',
+	  'texas': 'TX',
+	  'utah': 'UT',
+	  'vermont': 'VT',
+	  'virginia': 'VA',
+	  'washington': 'WA',
+	  'west virginia': 'WV',
+	  'wisconsin': 'WI',
+	  'wyoming': 'WY'
 	
 }
 
@@ -76,6 +72,7 @@ function getDates(startDate, endDate) {
 
 let Controller = (() => {
 	let state = 'Alabama';
+	let stateAbbr = states;
 
 	let HTML = {
 		selectStates: 'state',
@@ -123,6 +120,9 @@ let Controller = (() => {
 				console.log('cases: ' + cases + ' deaths: ' + deaths + ' recovered: ' + recovered);
 				
 			}	
+		},
+		setImg() {
+			document.getElementById(HTML.stateDiv).getElementsByTagName('img')[0].src = '../res/State-flags/' + stateAbbr[state.toLowerCase()].toLowerCase() + '.png';
 		}
 
 	}
@@ -140,7 +140,10 @@ window.onload = function() {
 		// add your event listeners
 		document.getElementById(HTML.selectStates).addEventListener('change', (event) => {
 			Controller.setState(event.target.value);
+			alert('test');
+			Controller.setImg();
 			Controller.setTotalStatistics();
+			
 		})
 	};
 
